0c34d4c71567336fb34ad89161754ba698307865
TimaFrolov <tima1911@yandex.ru>
2023-09-19 23:33:44 +0300

    Add solution for hw2 (without tests and docs)

---
+++ hw2/Lazy/Lazy.cs
@@ -0,0 +1,65 @@
+﻿namespace Lazy;
+using Utils;
+
+public interface ILazy<T>
+{
+    T Get();
+}
+
+public sealed class Lazy<T> : ILazy<T>
+{
+    private Option<Func<T>> factory;
+    private Option<Result<T, Exception>> value = Option<Result<T, Exception>>.None;
+
+    public Lazy(Func<T> factory) => this.factory = factory;
+
+    public T Get()
+    {
+        this.factory.Map(func =>
+        {
+            this.value = Try<Exception>.Call(this.factory.Unwrap());
+            this.factory = Option<Func<T>>.None;
+        });
+
+        return this.value.Unwrap().UnwrapOrElse(err => throw err);
+    }
+}
+
+public sealed class AtomicLazy<T> : ILazy<T>
+{
+    private Option<Func<T>> factory;
+    private Option<Result<T, Exception>> value = Option<Result<T, Exception>>.None;
+
+    private int valueStartedProducing = 0;
+    private bool valueProduced = false;
+    private ManualResetEvent valueProducedEvent = new ManualResetEvent(false);
+
+    public AtomicLazy(Func<T> factory) => this.factory = factory;
+
+    public T Get()
+    {
+        if (Interlocked.Increment(ref this.valueStartedProducing) == 1)
+        {
+            Result<T, Exception> value = Try<Exception>.Call(this.factory.Unwrap());
+
+            this.value = value;
+            Volatile.Write(ref this.valueProduced, true);
+            this.valueProducedEvent.Set();
+
+            this.factory = Option<Func<T>>.None;
+            return value.UnwrapOrElse(err => throw err);
+        }
+
+        Interlocked.Exchange(ref this.valueStartedProducing, 1);
+
+        if (!this.valueProduced)
+        {
+            this.valueProducedEvent.WaitOne();
+            return Volatile.Read(ref this.value).Unwrap().UnwrapOrElse(err => throw err);
+        }
+        else
+        {
+            return this.value.Unwrap().UnwrapOrElse(err => throw err);
+        }
+    }
+}
