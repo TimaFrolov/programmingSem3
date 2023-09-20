a82ec8eecb32708dae86037526e435753e625dd7
TimaFrolov <tima1911@yandex.ru>
2023-09-20 21:28:13 +0300

    Add documentation for matrix

--- hw1/MatrixMultiplication/Matrix.cs
+++ hw1/MatrixMultiplication/Matrix.cs
@@ -1,22 +1,43 @@
 namespace MatrixMultiplication;
 
+/// <summary> Two dimensional matrix with integer elements. </summary>
 public class IntMatrix
 {
     private int[,] underlying;
 
+    /// <summary> Gets height of the matrix. </summary>
     public int Height => underlying.GetLength(0);
+
+    /// <summary> Gets width of the matrix. </summary>
     public int Width => underlying.GetLength(1);
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IntMatrix"/> class from given matrix.
+    /// </summary>
+    /// <param name="matrix">Underlying matrix.</param>
     public IntMatrix(int[,] matrix) => this.underlying = matrix;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IntMatrix"/> class with given dimensions.
+    /// </summary>
+    /// <param name="height">Height of the matrix.</param>
+    /// <param name="width">Width of the matrix.</param>
     public IntMatrix(int height, int width) => this.underlying = new int[height, width];
 
-    public int this[int i, int j]
+    /// <summary> Gets or sets element of the matrix with given position.</summary>
+    /// <param name="row">Row index of the element.</param>
+    /// <param name="column">Column index of the element.</param>
+    public int this[int row, int column]
     {
-        get => this.underlying[i, j];
-        set => this.underlying[i, j] = value;
+        get => this.underlying[row, column];
+        set => this.underlying[row, column] = value;
     }
 
+    /// <summary>
+    /// Compares two matrices.
+    /// </summary>
+    /// <param name="other">Matrix to compare with.</param>
+    /// <returns>True if matrices are equal, false otherwise.</returns>
     public override bool Equals(object? other)
     {
         if (!(other is IntMatrix matrix))
