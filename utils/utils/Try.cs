8b66686a5d4c326b9a3fdc1ca69bcd268ee4c15e
TimaFrolov <tima1911@yandex.ru>
2023-09-20 13:51:42 +0300

    Add docs for util types

--- utils/utils/Try.cs
+++ utils/utils/Try.cs
@@ -1,8 +1,21 @@
 namespace Utils;
 
+/// <summary>
+/// Class for trying to call a function that may throw an exception.
+/// </summary>
+/// <typeparam name="TException">The type of the exception.</typeparam>
 public static class Try<TException>
 where TException : Exception
 {
+    /// <summary>
+    /// Calls a function that may throw an exception.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    /// <param name="func">The function.</param>
+    /// <returns>
+    /// If the function throws an exception, <see cref="Result{TResult, TException}.Err"/> with the exception,
+    /// <see cref="Result{TResult, TException}.Ok"/> with the result otherwise.
+    /// </returns>
     public static Result<TResult, TException> Call<TResult>(Func<TResult> func)
     {
         try
