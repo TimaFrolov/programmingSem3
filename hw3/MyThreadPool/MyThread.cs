7d84c98b6178542d003d8f8364d4cc91c868b46a
TimaFrolov <tima1911@yandex.ru>
2023-10-05 20:54:39 +0300

    Fix warnings (=> also add documentation)

--- hw3/MyThreadPool/MyThread.cs
+++ hw3/MyThreadPool/MyThread.cs
@@ -1,14 +1,19 @@
-using System.Collections.Concurrent;
+namespace MyThreadPool;
 
+/// <summary>
+/// Represents a thread that executes tasks.
+/// </summary>
 internal class MyThread
 {
-    public bool IsBusy { get; private set; } = false;
-
     private Thread underlying;
     private Func<Action> taskFactory;
 
     private volatile bool keepExecuting = true;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MyThread"/> class with the specified task factory.
+    /// </summary>
+    /// <param name="taskFactory">The task factory.</param>
     public MyThread(Func<Action> taskFactory)
     {
         this.taskFactory = taskFactory;
@@ -16,18 +21,21 @@ internal class MyThread
         this.underlying.Start();
     }
 
+    /// <summary>
+    /// Shuts down the thread.
+    /// </summary>
+    public void Shutdown() => this.keepExecuting = false;
+
+    /// <summary>
+    /// Blocks until the thread is finished.
+    /// </summary>
+    public void Wait() => this.underlying.Join();
+
     private void EventLoop()
     {
         while (this.keepExecuting)
         {
-            Action task = this.taskFactory();
-            this.IsBusy = true;
-            task();
-            this.IsBusy = false;
+            this.taskFactory()();
         }
     }
-
-    public void Shutdown() => this.keepExecuting = false;
-
-    public void Wait() => this.underlying.Join();
 }
--- hw3/MyThreadPool/MyThreadPool.cs
+++ hw3/MyThreadPool/MyThreadPool.cs
@@ -1,27 +1,24 @@
-﻿using System.Collections.Concurrent;
+﻿namespace MyThreadPool;
+
+using System.Collections.Concurrent;
 using Utils;
 
-namespace MyThreadPool;
-
+/// <summary>
+/// A thread pool that executes tasks in parallel.
+/// </summary>
 public class MyThreadPool
 {
-    private interface Executable
-    {
-        public void Execute();
-    }
-
-    private class EmptyExecutable : Executable
-    {
-        public void Execute() { }
-    }
-
     private MyThread[] threads;
-    private ConcurrentQueue<Executable> tasks = new();
+    private ConcurrentQueue<IExecutable> tasks = new();
     private Semaphore tasksAmount = new(0, int.MaxValue);
     private volatile bool keepExecuting = true;
     private ManualResetEvent queueIsEmpty = new(false);
     private volatile int tasksToEnqueue = 0;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MyThreadPool"/> class with the specified amount of threads.
+    /// </summary>
+    /// <param name="threadsAmount">The amount of threads to create.</param>
     public MyThreadPool(int threadsAmount)
     {
         this.threads = Enumerable
@@ -30,25 +27,24 @@ public class MyThreadPool
             .ToArray();
     }
 
-    private Action ProvideTask()
+    /// <summary>
+    /// Represents a task that is executed by a thread pool.
+    /// </summary>
+    private interface IExecutable
     {
-        this.tasksAmount.WaitOne();
-        this.tasks.TryDequeue(out Executable? task);
-        this.UpdateQueueIsEmpty();
-        return task!.Execute;
+        /// <summary>
+        /// Executes the task.
+        /// </summary>
+        public void Execute();
     }
 
-    private void UpdateQueueIsEmpty()
-    {
-        lock (this.tasks)
-        {
-            if (!this.keepExecuting && this.tasks.IsEmpty && this.tasksToEnqueue == 0)
-            {
-                this.queueIsEmpty.Set();
-            }
-        }
-    }
-
+    /// <summary>
+    /// Submits a task to the thread pool.
+    /// </summary>
+    /// <typeparam name="T">The type of the task's result.</typeparam>
+    /// <param name="task">The task to submit.</param>
+    /// <returns>A task that represents the submitted task.</returns>
+    /// <throws><see cref="InvalidOperationException"/> if the thread pool is shut down.</throws>
     public IMyTask<T> Submit<T>(Func<T> task)
     {
         lock (this.tasks)
@@ -65,6 +61,12 @@ public class MyThreadPool
         }
     }
 
+    /// <summary>
+    /// Submits a task to the thread pool.
+    /// </summary>
+    /// <param name="task">The task to submit.</param>
+    /// <returns>A task that represents the submitted task.</returns>
+    /// <throws><see cref="InvalidOperationException"/> if the thread pool is shut down.</throws>
     public IMyTask Submit(Action task)
     {
         Task myTask;
@@ -78,17 +80,14 @@ public class MyThreadPool
             myTask = new(this, task);
             this.tasks.Enqueue(myTask);
         }
+
         this.tasksAmount.Release();
         return myTask;
     }
 
-    private void Submit(Executable task)
-    {
-        this.tasks.Enqueue(task);
-        this.tasksAmount.Release();
-        Interlocked.Decrement(ref this.tasksToEnqueue);
-    }
-
+    /// <summary>
+    /// Shuts down the thread pool. The method blocks until all tasks are completed and all threads are shut down.
+    /// </summary>
     public void Shutdown()
     {
         this.keepExecuting = false;
@@ -103,6 +102,7 @@ public class MyThreadPool
             thread.Shutdown();
             this.tasks.Enqueue(new EmptyExecutable());
         }
+
         this.tasksAmount.Release(this.threads.Length);
 
         foreach (var thread in this.threads)
@@ -111,11 +111,43 @@ public class MyThreadPool
         }
     }
 
-    public void Dispose()
+    /// <summary>
+    /// Shuts down the thread pool before it is garbage collected.
+    /// </summary>
+    public void Dispose() => this.Shutdown();
+
+    private void Submit(IExecutable task)
     {
-        this.Shutdown();
-        this.queueIsEmpty.Dispose();
-        this.tasksAmount.Dispose();
+        this.tasks.Enqueue(task);
+        this.tasksAmount.Release();
+        Interlocked.Decrement(ref this.tasksToEnqueue);
+    }
+
+    private Action ProvideTask()
+    {
+        this.tasksAmount.WaitOne();
+        this.tasks.TryDequeue(out IExecutable? task);
+        this.UpdateQueueIsEmpty();
+        return task!.Execute;
+    }
+
+    private void UpdateQueueIsEmpty()
+    {
+        lock (this.tasks)
+        {
+            if (!this.keepExecuting && this.tasks.IsEmpty && this.tasksToEnqueue == 0)
+            {
+                this.queueIsEmpty.Set();
+            }
+        }
+    }
+
+    private class EmptyExecutable : IExecutable
+    {
+        /// <summary>
+        /// Does literally nothing.
+        /// </summary>
+        public void Execute() { }
     }
 
     private class Task : Task<Monostate>, IMyTask
@@ -128,29 +160,49 @@ public class MyThreadPool
                     task();
                     return Monostate.Instance;
                 }
-            ) { }
+            )
+        { }
 
         public IMyTask<TNew> ContinueWith<TNew>(Func<TNew> continuation) =>
             this.ContinueWith((Monostate _) => continuation());
+
+        public IMyTask ContinueWith(Action continuation) =>
+            this.ContinueWith((Monostate _) => continuation());
     }
 
-    private class Task<T> : IMyTask<T>, Executable
+    private class Task<T> : IMyTask<T>, IExecutable
     {
         private MyThreadPool threadPool;
-
         private bool isCompleted = false;
         private Option<Result<T, Exception>> result = Option<Result<T, Exception>>.None;
         private ManualResetEvent executeCompleted = new(false);
         private Option<Func<T>> task;
-
-        private List<Executable> continuations = new();
-
-        public bool IsCompleted => this.isCompleted;
+        private List<IExecutable> continuations = new();
 
         public Task(MyThreadPool threadPool, Func<T> task)
         {
             this.task = task;
             this.threadPool = threadPool;
+        }
+
+        public bool IsCompleted => this.isCompleted;
+
+        /// <inheritdoc cref="IMyTask{T}.Result"/>
+        public T Result
+        {
+            get
+            {
+                if (this.isCompleted)
+                {
+                    return this.result.Unwrap().UnwrapOrElse(e => throw new AggregateException(e));
+                }
+
+                this.executeCompleted.WaitOne();
+                return Volatile
+                    .Read(ref this.result)
+                    .Unwrap()
+                    .UnwrapOrElse(e => throw new AggregateException(e));
+            }
         }
 
         public void Execute()
@@ -173,26 +225,25 @@ public class MyThreadPool
             }
         }
 
-        public T Result
-        {
-            get
-            {
-                if (this.isCompleted)
-                {
-                    return this.result.Unwrap().UnwrapOrElse(e => throw new AggregateException(e));
-                }
-
-                this.executeCompleted.WaitOne();
-                return Volatile
-                    .Read(ref this.result)
-                    .Unwrap()
-                    .UnwrapOrElse(e => throw new AggregateException(e));
-            }
-        }
-
         public void Wait() => this.executeCompleted.WaitOne();
 
         public IMyTask<TNew> ContinueWith<TNew>(Func<T, TNew> continuation)
+        {
+            this.IncrementTaskAmount();
+            Task<TNew> newTask = new(this.threadPool, () => continuation(this.Result));
+            this.SubmitContinuation(newTask);
+            return newTask;
+        }
+
+        public IMyTask ContinueWith(Action<T> continuation)
+        {
+            this.IncrementTaskAmount();
+            Task newTask = new(this.threadPool, () => continuation(this.Result));
+            this.SubmitContinuation(newTask);
+            return newTask;
+        }
+
+        private void IncrementTaskAmount()
         {
             Interlocked.Increment(ref this.threadPool.tasksToEnqueue);
             if (!this.threadPool.keepExecuting)
@@ -201,20 +252,21 @@ public class MyThreadPool
                 this.threadPool.UpdateQueueIsEmpty();
                 throw new InvalidOperationException("The thread pool is shut down.");
             }
-            Task<TNew> newTask = new(this.threadPool, () => continuation(this.Result));
+        }
+
+        private void SubmitContinuation(IExecutable task)
+        {
             lock (this.result)
             {
                 if (Volatile.Read(ref this.isCompleted))
                 {
-                    this.threadPool.Submit(newTask);
+                    this.threadPool.Submit(task);
                 }
                 else
                 {
-                    this.continuations.Add(newTask);
+                    this.continuations.Add(task);
                 }
             }
-
-            return newTask;
         }
     }
 }
