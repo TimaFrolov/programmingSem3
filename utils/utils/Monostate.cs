86f66fa90b82d813c5046adf0f168a0e3476ceae
TimaFrolov <tima1911@yandex.ru>
2023-10-05 20:59:22 +0300

    Add docs for monostate class

--- utils/utils/Monostate.cs
+++ utils/utils/Monostate.cs
@@ -1,5 +1,11 @@
+/// <summary>
+/// Class that has one instance.
+/// </summary>
 public class Monostate
 {
+    /// <summary>
+    /// Instance of the class.
+    /// </summary>
     public static Monostate Instance { get; } = new();
 
     private Monostate() { }
