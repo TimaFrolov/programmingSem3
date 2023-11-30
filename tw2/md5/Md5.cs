b682488cd3e01c067871dd099376760dfbbc04ee
TimaFrolov <tima1911@yandex.ru>
2023-11-30 18:05:52 +0300

    Add tests and write documentation

--- tw2/md5/Md5.cs
+++ tw2/md5/Md5.cs
@@ -1,13 +1,15 @@
 namespace Md5;
 
+/// <summary>
+/// Static class providing methods for hashing directories using md5 hash function.
+/// </summary>
 public static class Md5Hasher
 {
-    private static async Task<byte[]> HashFileAsync(FileInfo file) =>
-        await System.Security.Cryptography.MD5.HashDataAsync(file.OpenRead());
-
-    private static byte[] HashFile(FileInfo file) =>
-        System.Security.Cryptography.MD5.HashData(file.OpenRead());
-
+    /// <summary>
+    /// Hashes directory synchronously.
+    /// </summary>
+    /// <param name="directory"> Directory to hash.</param>
+    /// <returns> Hash of directory.</returns>
     public static byte[] HashDirectory(DirectoryInfo directory)
     {
         MemoryStream stream = new();
@@ -24,10 +26,18 @@ public static class Md5Hasher
             stream.Write(hash);
         }
 
-        stream.Flush();
-        return System.Security.Cryptography.MD5.HashData(stream.GetBuffer());
+        return System
+            .Security
+            .Cryptography
+            .MD5
+            .HashData(stream.GetBuffer().Take((int)stream.Position).ToArray());
     }
 
+    /// <summary>
+    /// Hashes directory asynchronously.
+    /// </summary>
+    /// <param name="directory"> Directory to hash.</param>
+    /// <returns> Hash of directory.</returns>
     public static async Task<byte[]> HashDirectoryAsync(DirectoryInfo directory)
     {
         MemoryStream stream = new();
@@ -46,6 +56,16 @@ public static class Md5Hasher
             stream.Write(hash);
         }
 
-        return System.Security.Cryptography.MD5.HashData(stream.GetBuffer());
+        return System
+            .Security
+            .Cryptography
+            .MD5
+            .HashData(stream.GetBuffer().Take((int)stream.Position).ToArray());
     }
+
+    private static async Task<byte[]> HashFileAsync(FileInfo file) =>
+        await System.Security.Cryptography.MD5.HashDataAsync(file.OpenRead());
+
+    private static byte[] HashFile(FileInfo file) =>
+        System.Security.Cryptography.MD5.HashData(file.OpenRead());
 }
--- tw2/md5/Program.cs
+++ tw2/md5/Program.cs
@@ -1,7 +1,15 @@
 ﻿namespace Md5;
 
+/// <summary>
+/// Program class.
+/// </summary>
 internal class Program
 {
+    /// <summary>
+    /// Main function of the program.
+    /// </summary>
+    /// <param name="args"> Console arguments.</param>
+    /// <returns> Exit code.</returns>
     public static int Main(string[] args)
     {
         if (args.Length != 1)
@@ -20,7 +28,7 @@ internal class Program
             byte[] hash = Md5Hasher.HashDirectory(new DirectoryInfo(args[0]));
             stopwatch.Stop();
             Console.WriteLine(
-                $"Synchrounous algorithm result: {string.Join("", hash.Select(x => $"{x:x2}"))}. "
+                $"Synchrounous algorithm result: {string.Join(string.Empty, hash.Select(x => $"{x:x2}"))}. "
                     + $"Elapsed time: {stopwatch.ElapsedMilliseconds} ms"
             );
 
@@ -28,7 +36,7 @@ internal class Program
             byte[] asyncHash = Md5Hasher.HashDirectoryAsync(new DirectoryInfo(args[0])).Result;
             stopwatch.Stop();
             Console.WriteLine(
-                $"Asynchrounous algorithm result: {string.Join("", asyncHash.Select(x => $"{x:x2}"))}. "
+                $"Asynchrounous algorithm result: {string.Join(string.Empty, asyncHash.Select(x => $"{x:x2}"))}. "
                     + $"Elapsed time: {stopwatch.ElapsedMilliseconds} ms"
             );
         }
